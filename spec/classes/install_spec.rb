63e549cdfb0698f063c4cdf286d122e54fcb7bbd
Mike Bruzina <mbruzina@newrelic.com>
2023-01-30 09:50:06 -0600

    regx for testing exec command attrs

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -23,15 +23,16 @@ describe 'newrelic_installer::install' do
 
       # kernel-specific asserts
       if os_facts[:kernel] == 'Linux'
-        it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.sh', 'ensure' => 'present', 'mode' => '777') }
-        it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').that_comes_before('Exec[install newrelic-cli]') }
+        it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.sh', 'ensure' => 'present', 'mode' => '777').that_comes_before('Exec[install newrelic-cli]') }
+        it { is_expected.to contain_exec('install newrelic instrumentation').with('command' => %r{(.*)newrelic install(.*)--tag nr_deployed_by:puppet-install}) }
       elsif os_facts[:kernel] == 'windows'
-        it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1', 'ensure' => 'present', 'mode' => '777') }
-        it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').that_comes_before('Exec[install newrelic-cli]') }
+        it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1', 'ensure' => 'present', 'mode' => '777').that_comes_before('Exec[install newrelic-cli]') }
+        it { is_expected.to contain_exec('install newrelic instrumentation').with('command' => %r{(.*)newrelic.exe" install(.*)--tag nr_deployed_by:puppet-install}) }
       end
+
       # common asserts
-      it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
       it { is_expected.to have_exec_resource_count(2) }
+      it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
       it { is_expected.to contain_service('newrelic-infra').only_with('ensure' => 'running', 'enable' => true) }
     end
   end
@@ -89,36 +90,36 @@ describe 'newrelic_installer::install' do
       it { is_expected.to compile.and_raise_error(%r{New Relic region not provided}) }
     end
   end
-  on_supported_os.each do |os, os_facts|
-    # filters only Linux kernel, reads a bit odd
-    # next unless os_facts[:kernel] == 'Linux'
-    context "installed with optional tags on #{os}" do
-      let(:facts) { os_facts }
-      let(:params) do
-        {
-          'targets' => ['some-valid-recipe-name'],
-          'environment_variables' => {
-            'NEW_RELIC_API_KEY' => 'some-api-key',
-            'NEW_RELIC_ACCOUNT_ID' => 123,
-            'NEW_RELIC_REGION' => 'some-region'
-          },
-          'verbosity' => 'debug',
-          'proxy' => ''
-        }
-      end
-
-      # kernel-specific asserts
-      if os_facts[:kernel] == 'Linux'
-        it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.sh', 'ensure' => 'present', 'mode' => '777') }
-        it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').that_comes_before('Exec[install newrelic-cli]') }
-      elsif os_facts[:kernel] == 'windows'
-        it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1', 'ensure' => 'present', 'mode' => '777') }
-        it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').that_comes_before('Exec[install newrelic-cli]') }
-      end
-      # common asserts
-      it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
-      it { is_expected.to have_exec_resource_count(2) }
-      it { is_expected.to contain_service('newrelic-infra').only_with('ensure' => 'running', 'enable' => true) }
-    end
-  end
+  # on_supported_os.each do |os, os_facts|
+  #   # filters only Linux kernel, reads a bit odd
+  #   # next unless os_facts[:kernel] == 'Linux'
+  #   context "installed with optional tags on #{os}" do
+  #     let(:facts) { os_facts }
+  #     let(:params) do
+  #       {
+  #         'targets' => ['some-valid-recipe-name'],
+  #         'environment_variables' => {
+  #           'NEW_RELIC_API_KEY' => 'some-api-key',
+  #           'NEW_RELIC_ACCOUNT_ID' => 123,
+  #           'NEW_RELIC_REGION' => 'some-region'
+  #         },
+  #         'verbosity' => 'debug',
+  #         'proxy' => ''
+  #       }
+  #     end
+  #
+  #     # kernel-specific asserts
+  #     if os_facts[:kernel] == 'Linux'
+  #       it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.sh', 'ensure' => 'present', 'mode' => '777') }
+  #       it { is_expected.to contain_remote_file('/tmp/newrelic_cli_install.sh').that_comes_before('Exec[install newrelic-cli]') }
+  #     elsif os_facts[:kernel] == 'windows'
+  #       it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1', 'ensure' => 'present', 'mode' => '777') }
+  #       it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').that_comes_before('Exec[install newrelic-cli]') }
+  #     end
+  #     # common asserts
+  #     it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
+  #     it { is_expected.to have_exec_resource_count(2) }
+  #     it { is_expected.to contain_service('newrelic-infra').only_with('ensure' => 'running', 'enable' => true) }
+  #   end
+  # end
 end
