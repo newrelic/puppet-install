61d5dc2fe567f7bce1cfed4bd400d0d603d0b63b
Mike Bruzina <mbruzina@newrelic.com>
2023-01-30 09:19:34 -0600

    fixed test indentations

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -29,64 +29,63 @@ describe 'newrelic_installer::install' do
         it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').with('source' => 'https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1', 'ensure' => 'present', 'mode' => '777') }
         it { is_expected.to contain_remote_file('C:\Windows\TEMP\install.ps1').that_comes_before('Exec[install newrelic-cli]') }
       end
+      # common asserts
+      it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
+      it { is_expected.to have_exec_resource_count(2) }
+      it { is_expected.to contain_service('newrelic-infra').only_with('ensure' => 'running', 'enable' => true) }
     end
-    # common asserts
-    it { is_expected.to contain_exec('install newrelic-cli').that_comes_before('Exec[install newrelic instrumentation]') }
-    it { is_expected.to have_exec_resource_count(2) }
-    it { is_expected.to contain_service('newrelic-infra').only_with('ensure' => 'running', 'enable' => true) }
   end
-end
-on_supported_os.each do |os, os_facts|
-  context "failing with missing api key on #{os}" do
-    let(:facts) { os_facts }
-    let(:params) do
-      {
-        'targets' => ['some-valid-recipe-name'],
-        'environment_variables' => {
-          'NEW_RELIC_ACCOUNT_ID' => 123,
-          'NEW_RELIC_REGION' => 'some-region'
-        },
-        'verbosity' => 'loud',
-        'proxy' => 'some-valid-url'
-      }
-    end
+  on_supported_os.each do |os, os_facts|
+    context "failing with missing api key on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          'targets' => ['some-valid-recipe-name'],
+          'environment_variables' => {
+            'NEW_RELIC_ACCOUNT_ID' => 123,
+            'NEW_RELIC_REGION' => 'some-region'
+          },
+          'verbosity' => 'loud',
+          'proxy' => 'some-valid-url'
+        }
+      end
 
-    it { is_expected.to compile.and_raise_error(%r{New Relic api key not provided}) }
+      it { is_expected.to compile.and_raise_error(%r{New Relic api key not provided}) }
+    end
   end
-end
-on_supported_os.each do |os, os_facts|
-  context "failing with missing account id on #{os}" do
-    let(:facts) { os_facts }
-    let(:params) do
-      {
-        'targets' => ['some-valid-recipe-name'],
-        'environment_variables' => {
-          'NEW_RELIC_API_KEY' => 'some-api-key',
-          'NEW_RELIC_REGION' => 'some-region'
-        },
-        'verbosity' => '',
-        'proxy' => ''
-      }
-    end
+  on_supported_os.each do |os, os_facts|
+    context "failing with missing account id on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          'targets' => ['some-valid-recipe-name'],
+          'environment_variables' => {
+            'NEW_RELIC_API_KEY' => 'some-api-key',
+            'NEW_RELIC_REGION' => 'some-region'
+          },
+          'verbosity' => '',
+          'proxy' => ''
+        }
+      end
 
-    it { is_expected.to compile.and_raise_error(%r{New Relic account ID not provided}) }
+      it { is_expected.to compile.and_raise_error(%r{New Relic account ID not provided}) }
+    end
   end
-end
-on_supported_os.each do |os, os_facts|
-  context "failing with missing region on #{os}" do
-    let(:facts) { os_facts }
-    let(:params) do
-      {
-        'targets' => ['some-valid-recipe-name'],
-        'environment_variables' => {
-          'NEW_RELIC_API_KEY' => 'some-api-key',
-          'NEW_RELIC_ACCOUNT_ID' => 123,
-        },
-        'verbosity' => '',
-        'proxy' => ''
-      }
+  on_supported_os.each do |os, os_facts|
+    context "failing with missing region on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          'targets' => ['some-valid-recipe-name'],
+          'environment_variables' => {
+            'NEW_RELIC_API_KEY' => 'some-api-key',
+            'NEW_RELIC_ACCOUNT_ID' => 123,
+          },
+          'verbosity' => '',
+          'proxy' => ''
+        }
+      end
+      it { is_expected.to compile.and_raise_error(%r{New Relic region not provided}) }
     end
-
-    it { is_expected.to compile.and_raise_error(%r{New Relic region not provided}) }
   end
 end
