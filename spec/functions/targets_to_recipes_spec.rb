0727f7328ead520e211baca5c17d375b4671d7c4
NSSPKrishna <pniladhuri@newrelic.com>
2023-05-31 20:19:19 +0530

    feat(dotnet): Add dotnet instrumentation option

--- spec/functions/targets_to_recipes_spec.rb
+++ spec/functions/targets_to_recipes_spec.rb
@@ -6,6 +6,7 @@ describe 'newrelic_installer::targets_to_recipes' do
   # single valid recipe - infrastructure
   it { is_expected.to run.with_params(['infrastructure']).and_return('infrastructure-agent-installer') }
   it { is_expected.to run.with_params(['php']).and_return('php-agent-installer') }
+  it { is_expected.to run.with_params(['dotnet']).and_return('dotnet-agent-installer') }
   # valid and invalid
   it { is_expected.to run.with_params(['infrastructure', 'nope']).and_return('infrastructure-agent-installer') }
   # all invalid
@@ -14,4 +15,5 @@ describe 'newrelic_installer::targets_to_recipes' do
   it { is_expected.to run.with_params(['logs']).and_raise_error(Puppet::ParseError) }
   # infrastructure is still missing
   it { is_expected.to run.with_params(['php', 'logs']).and_raise_error(Puppet::ParseError) }
+  it { is_expected.to run.with_params(['dotnet', 'logs']).and_raise_error(Puppet::ParseError) }
 end
